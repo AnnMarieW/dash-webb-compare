78fc0999ad406d4f6990bf84c5160cb5de89204c
AnnMarieW <amward@fastmail.com>
2022-08-08 17:17:48 -0700

    trying out new responsive image slider

--- src/app.py
+++ src/app.py
@@ -5,8 +5,9 @@ This is an example of a multi-page app made with `pages` that does not use the p
 
 import dash
 from dash import Dash, dcc, html, Output, Input
-from dash_extensions import BeforeAfter
+#from dash_extensions import BeforeAfter
 import dash_bootstrap_components as dbc
+from before_after import BeforeAfter
 
 app = Dash(
     __name__,
@@ -24,10 +25,10 @@ def make_before_after(before, after):
             html.Div(
                 [html.Div("Hubble"), html.Div("Webb")],
                 className="d-flex justify-content-between",
-                style={"width": 1000},
+                style={"width": "auto"},
                 id="label_div"
             ),
-            BeforeAfter(before=before, after=after, height=800, width=1000, id="before_after"),
+            BeforeAfter(before=before, after=after, id="before_after"),
         ],
         style={"marginTop": 50},
     )
@@ -41,7 +42,7 @@ dash.register_page(
     description=descr,
     image="webb_stephans_quintet_350.jpg",
     layout=make_before_after(
-        "/assets/webb_stephans_quintet.jpg", "/assets/stephans_quintet.jpg"
+        "/assets/stephans_quintet.jpg", "/assets/webb_stephans_quintet.jpg"
     ),
 )
 
@@ -50,7 +51,7 @@ dash.register_page(
     name="Galaxy Cluster SMACS 0723",
     description=descr,
     image="deep_field_meta.gif",
-    layout=make_before_after("/assets/webb_deep_field.jpg", "/assets/deep_field.jpg"),
+    layout=make_before_after("/assets/deep_field.jpg", "/assets/webb_deep_field.jpg"),
     path="/",
 )
 
@@ -59,7 +60,7 @@ dash.register_page(
     name="Carina Nebula",
     description=descr,
     image="webb_carina_350.jpg",
-    layout=make_before_after("/assets/webb_carina.jpg", "/assets/carina.png"),
+    layout=make_before_after("/assets/carina.png", "/assets/webb_carina.jpg"),
 )
 dash.register_page(
     "webb_southern_ring",
@@ -67,7 +68,7 @@ dash.register_page(
     description=descr,
     image="webb_southern_nebula_350.jpg",
     layout=make_before_after(
-        "/assets/webb_southern_nebula.jpg", "/assets/southern_nebula.jpg"
+        "/assets/southern_nebula.jpg", "/assets/webb_southern_nebula.jpg"
     ),
 )
 
@@ -76,7 +77,7 @@ dash.register_page(
     name="Cartwheel Gallery",
     description=descr,
     image="webb_cartwheel_350.jpg",
-    layout=make_before_after("/assets/webb_cartwheel.png", "/assets/cartwheel.png"),
+    layout=make_before_after("/assets/cartwheel.png", "/assets/webb_cartwheel.png"),
 )
 
 header = html.Div(
@@ -114,38 +115,38 @@ def navbar():
     )
 
 app.layout = dbc.Container([header, navbar(), dash.page_container, dcc.Location(id="url")])
-
-
-app.clientside_callback(
-    """
-        function(href) {
-            if (window.innerWidth < 750) {
-                return [500, 500]
-            }
-            return [1000, 800]
-        }
-    """,
-    Output('before_after', "width"),
-    Output('before_after', "height"),
-    Input('url', 'href')
-)
-
-
-
-
-
-app.clientside_callback(
-    """
-        function(href) {
-            if (window.innerWidth < 750) {                
-                return {"width":500}
-            }
-             return {"width":1000}
-        }
-    """,
-    Output('label_div', "style"),
-    Input('url', 'href')
-)
+#
+#
+# app.clientside_callback(
+#     """
+#         function(href) {
+#             if (window.innerWidth < 750) {
+#                 return [500, 500]
+#             }
+#             return [1000, 800]
+#         }
+#     """,
+#     Output('before_after', "width"),
+#     Output('before_after', "height"),
+#     Input('url', 'href')
+# )
+#
+#
+#
+#
+#
+# app.clientside_callback(
+#     """
+#         function(href) {
+#             if (window.innerWidth < 750) {
+#                 return {"width":500}
+#             }
+#              return {"width":1000}
+#         }
+#     """,
+#     Output('label_div', "style"),
+#     Input('url', 'href')
+# )
 
 if __name__ == "__main__":
     app.run_server(debug=True)
