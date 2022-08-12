74cc67e16633845f6cc0e792652094cfe2c3bc8b
AnnMarieW <amward@fastmail.com>
2022-08-12 10:41:30 -0700

    Deploy to Heroku for app update - dashtools

--- src/app.py
+++ src/app.py
@@ -9,6 +9,17 @@ import dash_bootstrap_components as dbc
 from dash_before_after import BeforeAfter
 from whitenoise import WhiteNoise
 
+
+deep_field = "https://user-images.githubusercontent.com/72614349/179115661-f8de6e4c-0dca-4628-ab67-3d525f5ac049.jpg"
+stephans_quintet = "https://user-images.githubusercontent.com/72614349/179115662-32d348da-fa8b-481d-b4fc-9f7414f49de0.jpg"
+webb_stephans_quintet = "https://user-images.githubusercontent.com/72614349/179115663-71578706-1ab5-45a5-b809-812c7c3028a7.jpg"
+carina = "https://user-images.githubusercontent.com/72614349/179115665-9800b45c-e1dc-4aa7-8b34-885d48c61221.png"
+southern_nebula = "https://user-images.githubusercontent.com/72614349/179115666-fdd204fc-e33d-4524-9ba5-a2611740f8a7.jpg"
+webb_deep_field = "https://user-images.githubusercontent.com/72614349/179115668-2630e3e4-3a9f-4c88-9494-3412e606450a.jpg"
+webb_southern_nebula = "https://user-images.githubusercontent.com/72614349/179115670-ef5bc561-d957-4e88-82dc-53ca53541b04.jpg"
+webb_carina = "https://user-images.githubusercontent.com/72614349/179115673-15eaccb9-d17d-4667-84fb-e0a46fd444e8.jpg"
+
+
 app = Dash(
     __name__,
     use_pages=True,
@@ -43,7 +54,9 @@ dash.register_page(
     description=descr,
     image="webb_stephans_quintet_350.jpg",
     layout=make_before_after(
-        "/assets/stephans_quintet.jpg", "/assets/webb_stephans_quintet.jpg"
+        #"/assets/stephans_quintet.jpg", "/assets/webb_stephans_quintet.jpg"
+        stephans_quintet, webb_stephans_quintet
+
     ),
 )
 
@@ -52,7 +65,10 @@ dash.register_page(
     name="Galaxy Cluster SMACS 0723",
     description=descr,
     image="deep_field_meta.gif",
-    layout=make_before_after("/assets/deep_field.jpg", "/assets/webb_deep_field.jpg"),
+    layout=make_before_after(
+       # "/assets/deep_field.jpg", "/assets/webb_deep_field.jpg"
+        deep_field, webb_deep_field
+    ),
     path="/",
 )
 
@@ -61,7 +77,10 @@ dash.register_page(
     name="Carina Nebula",
     description=descr,
     image="webb_carina_350.jpg",
-    layout=make_before_after("/assets/carina.png", "/assets/webb_carina.jpg"),
+    layout=make_before_after(
+      #  "/assets/carina.png", "/assets/webb_carina.jpg"
+        carina, webb_carina
+    ),
 )
 dash.register_page(
     "webb_southern_ring",
@@ -69,7 +88,8 @@ dash.register_page(
     description=descr,
     image="webb_southern_nebula_350.jpg",
     layout=make_before_after(
-        "/assets/southern_nebula.jpg", "/assets/webb_southern_nebula.jpg"
+     #   "/assets/southern_nebula.jpg", "/assets/webb_southern_nebula.jpg"
+        southern_nebula, webb_southern_nebula
     ),
 )
 
